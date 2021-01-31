3fd8e448a909ad10d31fbe9edd436f30b1510878
王语曈 <alex@AlexdeMacBook-Pro.local>
2021-01-31 20:03:58 +0800

    Coding Exercise 2

---
+++ coding-exercises/coding-foundation/coding-exercise-2/README.md
@@ -0,0 +1,7 @@
+## Coding Exercise 2
+
+In this exercise I encountered
+- problems: I wanted to show the average score for each smell on the right side of the bar, instead of under the bar, but I failed.
+
+See my work [here](https://alexwang624.github.io/cdv-student/coding-exercises/coding-foundation/coding-exercise-2/).
+̨
---
+++ coding-exercises/coding-foundation/coding-exercise-2/script.js
@@ -0,0 +1,298 @@
+let data = [
+    {
+        "timestamp": "2021-01-29T11:48:12.258Z",
+        "Gasoline": 1,
+        "Rubbing Alcohol": 5,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 4,
+        "White Board Markers": 3,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 4
+    },
+    {
+        "timestamp": "2021-01-29T11:51:18.814Z",
+        "Gasoline": 4,
+        "Rubbing Alcohol": 4,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 3,
+        "White Board Markers": 1,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 1
+    },
+    {
+        "timestamp": "2021-01-29T11:51:46.504Z",
+        "Gasoline": 2,
+        "Rubbing Alcohol": 4,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 2,
+        "White Board Markers": 1,
+        "Nail Polish": 3,
+        "Underground Parking Garages": 1
+    },
+    {
+        "timestamp": "2021-01-29T11:58:03.882Z",
+        "Gasoline": 4,
+        "Rubbing Alcohol": 4,
+        "Old Books": 4,
+        "Chlorinated Swimming Pools": 2,
+        "White Board Markers": 2,
+        "Nail Polish": 2,
+        "Underground Parking Garages": 5
+    },
+    {
+        "timestamp": "2021-01-29T12:00:38.957Z",
+        "Gasoline": 2,
+        "Rubbing Alcohol": 4,
+        "Old Books": 3,
+        "Chlorinated Swimming Pools": 2,
+        "White Board Markers": 2,
+        "Nail Polish": 2,
+        "Underground Parking Garages": 1
+    },
+    {
+        "timestamp": "2021-01-29T12:03:47.917Z",
+        "Gasoline": 4,
+        "Rubbing Alcohol": 4,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 5,
+        "White Board Markers": 3,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 5
+    },
+    {
+        "timestamp": "2021-01-29T12:03:49.956Z",
+        "Gasoline": 2,
+        "Rubbing Alcohol": 4,
+        "Old Books": 3,
+        "Chlorinated Swimming Pools": 2,
+        "White Board Markers": 2,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 3
+    },
+    {
+        "timestamp": "2021-01-29T12:17:18.288Z",
+        "Gasoline": 4,
+        "Rubbing Alcohol": 1,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 5,
+        "White Board Markers": 2,
+        "Nail Polish": 4,
+        "Underground Parking Garages": 3
+    },
+    {
+        "timestamp": "2021-01-29T12:36:16.269Z",
+        "Gasoline": 3,
+        "Rubbing Alcohol": 3,
+        "Old Books": 3,
+        "Chlorinated Swimming Pools": 4,
+        "White Board Markers": 3,
+        "Nail Polish": 3,
+        "Underground Parking Garages": 3
+    },
+    {
+        "timestamp": "2021-01-29T12:38:37.371Z",
+        "Gasoline": 1,
+        "Rubbing Alcohol": 3,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 4,
+        "White Board Markers": 2,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 1
+    },
+    {
+        "timestamp": "2021-01-29T12:45:03.432Z",
+        "Gasoline": 4,
+        "Rubbing Alcohol": 3,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 2,
+        "White Board Markers": 2,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 5
+    },
+    {
+        "timestamp": "2021-01-29T12:53:14.611Z",
+        "Gasoline": 1,
+        "Rubbing Alcohol": 5,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 3,
+        "White Board Markers": 2,
+        "Nail Polish": 2,
+        "Underground Parking Garages": 1
+    },
+    {
+        "timestamp": "2021-01-29T13:35:40.113Z",
+        "Gasoline": 2,
+        "Rubbing Alcohol": 4,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 5,
+        "White Board Markers": 2,
+        "Nail Polish": 2,
+        "Underground Parking Garages": 5
+    },
+    {
+        "timestamp": "2021-01-29T13:40:33.426Z",
+        "Gasoline": 2,
+        "Rubbing Alcohol": 4,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 5,
+        "White Board Markers": 3,
+        "Nail Polish": 2,
+        "Underground Parking Garages": 1
+    },
+    {
+        "timestamp": "2021-01-29T13:50:21.398Z",
+        "Gasoline": 3,
+        "Rubbing Alcohol": 3,
+        "Old Books": 4,
+        "Chlorinated Swimming Pools": 2,
+        "White Board Markers": 2,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 3
+    },
+    {
+        "timestamp": "2021-01-29T13:56:21.640Z",
+        "Gasoline": 5,
+        "Rubbing Alcohol": 3,
+        "Old Books": 1,
+        "Chlorinated Swimming Pools": 1,
+        "White Board Markers": 4,
+        "Nail Polish": 2,
+        "Underground Parking Garages": 5
+    },
+    {
+        "timestamp": "2021-01-29T14:38:40.923Z",
+        "Gasoline": 1,
+        "Rubbing Alcohol": 2,
+        "Old Books": 4,
+        "Chlorinated Swimming Pools": 2,
+        "White Board Markers": 2,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 1
+    },
+    {
+        "timestamp": "2021-01-29T15:20:05.349Z",
+        "Gasoline": 1,
+        "Rubbing Alcohol": 2,
+        "Old Books": 3,
+        "Chlorinated Swimming Pools": 1,
+        "White Board Markers": 2,
+        "Nail Polish": 4,
+        "Underground Parking Garages": 5
+    },
+    {
+        "timestamp": "2021-01-29T15:23:10.686Z",
+        "Gasoline": 3,
+        "Rubbing Alcohol": 3,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 2,
+        "White Board Markers": 4,
+        "Nail Polish": 2,
+        "Underground Parking Garages": 5
+    },
+    {
+        "timestamp": "2021-01-29T16:45:48.851Z",
+        "Gasoline": 1,
+        "Rubbing Alcohol": 4,
+        "Old Books": 5,
+        "Chlorinated Swimming Pools": 5,
+        "White Board Markers": 1,
+        "Nail Polish": 2,
+        "Underground Parking Garages": 3
+    },
+    {
+        "timestamp": "2021-01-29T17:14:26.964Z",
+        "Gasoline": 2,
+        "Rubbing Alcohol": 2,
+        "Old Books": 4,
+        "Chlorinated Swimming Pools": 3,
+        "White Board Markers": 3,
+        "Nail Polish": 3,
+        "Underground Parking Garages": 2
+    },
+    {
+        "timestamp": "2021-01-29T17:48:57.310Z",
+        "Gasoline": 1,
+        "Rubbing Alcohol": 1,
+        "Old Books": 4,
+        "Chlorinated Swimming Pools": 2,
+        "White Board Markers": 2,
+        "Nail Polish": 2,
+        "Underground Parking Garages": 2
+    },
+    {
+        "timestamp": "2021-01-29T23:04:24.951Z",
+        "Gasoline": 1,
+        "Rubbing Alcohol": 4,
+        "Old Books": 2,
+        "Chlorinated Swimming Pools": 3,
+        "White Board Markers": 2,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 1
+    },
+    {
+        "timestamp": "2021-01-31T03:41:30.540Z",
+        "Gasoline": 4,
+        "Rubbing Alcohol": 3,
+        "Old Books": 4,
+        "Chlorinated Swimming Pools": 4,
+        "White Board Markers": 1,
+        "Nail Polish": 1,
+        "Underground Parking Garages": 5
+    }
+]
+
+function averageData(data){
+  let newData = [];
+  let keys = Object.keys(data[0]);
+  for(let i = 0; i < keys.length; i++){
+    let key = keys[i];
+    let sum = 0;
+    let num = 0;
+    for(let j = 0; j < data.length; j++){
+      let datum = data[j];
+      if(key in datum){
+        sum += datum[key];
+        num++;
+      }
+    }
+    let avg = sum/num;
+    if(!isNaN(avg)){
+      let newDataPoint = {"name": key, "average": avg, 'numMeasurements': num};
+      newData.push(newDataPoint);
+    }
+  }
+  return newData;
+}
+
+let container = document.getElementById("container");
+
+console.log("data", data);
+
+let transformedData = averageData(data);
+console.log("transformedData", transformedData);
+
+for (let i = 0; i < transformedData.length; i ++){
+  console.log("i", i);
+  let smellObject = transformedData[i];
+  console.log ("smellObject", smellObject);
+
+  let bar = document.createElement("div");
+  bar.className = "bar";
+  let barWidth = smellObject.average * 100;
+  bar.style.width = barWidth + "px";
+
+let name = smellObject.name;
+let ptag = document.createElement("p");
+ptag.innerHTML =  name;
+
+let number = document.createElement("div");
+number.className = "number";
+let numbertag = document.createElement("p");
+numbertag.innerHTML =  "Average Score:" + smellObject.average;
+
+bar.appendChild(ptag);
+number.appendChild(numbertag);
+
+container.appendChild(bar);
+container.appendChild(number);
+}
