f98d60f0a083725da047f161bd066e96d8bc87aa
AlexWang624 <yw4342@nyu.edu>
2021-02-24 00:47:54 +0800

    Exercise 4

---
+++ coding-exercises/coding-foundation/coding-exercise-4/README.md
@@ -0,0 +1,11 @@
+## Coding Exercise 3
+
+In this exercise, I created a simple visualization of the data I collected so far for the Data Zine project.
+
+- Problems:
+1. I encountered a problem with Google Sheets that it wouldn't let me export JSON files. The error message was "Exception: The number of rows in the range must be at least 1."
+
+- Things Learned:
+1. I learned how to create data visualization using D3.js.
+
+See my work [here](https://alexwang624.github.io/cdv-student/coding-exercises/coding-foundation/coding-exercise-3/).
---
+++ coding-exercises/coding-foundation/coding-exercise-4/script.js
@@ -0,0 +1,99 @@
+console.log("js loaded. hello!");
+
+let viz = d3.select("#viz-container")
+.append("svg") //append something to this selection
+   .attr("id", "viz") //id: name of this attribute, viz: value of this attribute
+   .attr("width", 600)
+   .attr("height", 500)
+;
+
+
+d3.json("data.json").then(gotData);
+
+function gotData(incomingData){
+  console.log(incomingData);
+    let bookWhere = incomingData[0].where;
+    console.log(bookWhere);
+
+
+viz.selectAll("circle").data(incomingData).enter()
+  .append("circle")
+  .attr("cx", a)
+  .attr("cy", 200)
+  .attr("r", 70)
+  .attr("fill", chooseColorWhere)
+  .attr("stroke", chooseColorLanguage)
+  .attr("stroke-width",15)
+  ;
+
+viz.selectAll("circle2").data(incomingData).enter()
+    .append("circle")
+    .attr("cx", a)
+    .attr("cy", 200)
+    .attr("r",40)
+    .attr("fill", chooseColorGender)
+    .attr("stroke", chooseColorDigital)
+    .attr("stroke-width",15)
+    ;
+
+viz.selectAll("rect").data(incomingData).enter()
+  .append("rect")
+  .attr("x", b)
+  .attr("y", 185)
+  .attr("width", 15)
+  .attr("height", 30)
+  .attr("fill", chooseColorPurpose)
+}
+;
+
+function chooseColorWhere(datapoint){
+  if (datapoint.where == "book"){
+    return "red"
+  }else if (datapoint.where == "essay"){
+    return "orange"
+  }
+}
+
+function chooseColorLanguage(datapoint){
+  if (datapoint.language == "chinese"){
+    return "blue"
+  }else if (datapoint.language == "english"){
+    return "pink"
+  }
+}
+
+function chooseColorDigital(datapoint){
+  if (datapoint.digitalOrPaper == "paper"){
+    return "white"
+  }else if (datapoint.digitalOrPaper == "digital"){
+    return "black"
+  }
+}
+
+function chooseColorGender(datapoint){
+  if (datapoint.gender == "male"){
+    return "green"
+  }else if (datapoint.gender == "female"){
+    return "purple"
+  }
+}
+
+function chooseColorPurpose(datapoint){
+  if (datapoint.purpose == "recreation"){
+    return "white"
+  }else if (datapoint.purpose == "academics"){
+    return "black"
+  }
+}
+
+function a(datapoint, i){
+    console.log("first value:", datapoint);
+    console.log("second value:", i);
+    return 100 + i * 200;
+}
+
+function b(datapoint, i){
+    console.log(datapoint);
+    console.log(i);
+    return 100 + i * 200 - 7;
+}
