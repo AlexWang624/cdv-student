861499102b6fb447b2b8b2f1352d51d7a82fda2e
AlexWang624 <yw4342@nyu.edu>
2021-05-10 03:03:58 +0800

    data story

--- projects/data-story/finished/script.js
+++ projects/data-story/finished/script.js
@@ -1,4 +1,4 @@
-let w = 2000;
+let w = 1300;
 let h = 1300;
 let padding = 90;
 let i = 0;
@@ -13,7 +13,7 @@ let viz = d3.select("#container")
 viz.append("text")
   .text("Space Odyssey")
   .attr("fill", "#625df6")
-  .attr("font-size", "250px")
+  .attr("font-size", "180px")
   .attr("font-family", "Abril Fatface")
   .attr("x", 0)
   .attr("y", h / 2 - 100)
@@ -24,13 +24,13 @@ viz.append("text")
 viz.append("text")
   .text("A Glimpse Into the Space Age")
   .attr("fill", "#625df6")
-  .attr("font-size", "100px")
+  .attr("font-size", "80px")
   .attr("font-family", "Abril Fatface")
   .attr("x", 0)
   .attr("y", h / 2 + 70);
 
 viz.append("text")
-  .text("A web-based data visualization project on the orbital launches in space from 1957.")
+  .text("A web-based data visualization project on the orbital")
   .attr("fill", "white")
   .attr("font-size", "40px")
   .attr("font-family", "'Roboto Mono', monospace")
@@ -38,14 +38,254 @@ viz.append("text")
   .attr("y", h / 2 + 200)
   .attr("overflow-wrap", "break-word");
 
-
+viz.append("text")
+  .text("launches in space from 1957.")
+  .attr("fill", "white")
+  .attr("font-size", "40px")
+  .attr("font-family", "'Roboto Mono', monospace")
+  .attr("x", 0)
+  .attr("y", h / 2 + 260)
+  .attr("overflow-wrap", "break-word");
 
 //Chronology
 let viz2 = d3.select("#chronology")
   .append("svg")
-  .attr("class", "viz2")
+  .attr("class", "viz4")
   .attr("width", w)
-  .attr("height", 1200)
+  .attr("height", 10400)
+  .style("background-color", "black");
+
+d3.json("Data/Chronology.json").then(function(timelineData) {
+  console.log(timelineData);
+  let datagroups = viz2.selectAll(".timelinedata").data(timelineData).enter()
+    .append("g")
+    .attr("class", "timelinedata");
+
+  let yTimeScale = d3.scaleLinear().domain([0, 27739]).range([60, 10040]);
+  let missionColorScale = d3.scaleLinear().domain([0, 27739]).range(["white", "#625df6"]);
+  let yYearScale = d3.scaleLinear().domain([1944, 2020]).range([20, 10000]);
+
+
+  function getVertical(d, i) {
+    console.log(d);
+    return yTimeScale(d.Day);
+  }
+
+
+  function getYearVertical(d, i) {
+    console.log(d);
+    return yTimeScale(d.Year);
+  }
+
+  function getyTimePosition(d, i) {
+    return 30 * i + 50;
+  }
+
+  function getMissionName(d, i) {
+    return d.Name + ", " + d.Mission + ", " + d.Country
+  }
+
+  function getDetailName(d, i) {
+    return "Mission Details: " + d.Mission
+  }
+
+  function getDate(d, i) {
+    return d.Date
+  }
+
+  function getMissionColor(d, i) {
+    return missionColorScale(d.Day);
+  }
+
+  function getpositionText(d, i) {
+    if (i % 2 === 0) {
+      return w / 2 + 230;
+    } else {
+      return w / 2 + 170;
+    }
+  }
+
+  function gettextpositionText(d, i) {
+    if (i % 2 === 0) {
+      return w / 2 + 410  ;
+    } else {
+      return w / 2;
+    }
+  }
+
+  function getpositionAnchor(d, i) {
+    if (i % 2 === 0) {
+      return "start";
+    } else {
+      return "end";
+    }
+  }
+
+
+  function wrap(text, width) {
+    text.each(function() {
+      var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        x = text.attr("x"),
+        y = text.attr("y"),
+        dy = 0, //parseFloat(text.attr("dy")),
+        tspan = text.text(null)
+        .append("tspan")
+        .attr("x", x)
+        .attr("y", y)
+        .attr("dy", dy + "em");
+      while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan")
+            .attr("x", x)
+            .attr("y", y)
+            .attr("dy", ++lineNumber * lineHeight + dy + "em")
+            .text(word);
+        }
+      }
+    });
+  }
+
+  datagroups.append("rect")
+    .style("fill", "white")
+    .attr("x", w / 2 + 200)
+    .attr("y", 60)
+    .attr("height", 9980)
+    .attr("width", 1)
+    .style("opacity", 1);
+
+  datagroups.append("text")
+    .attr("class", "datetext")
+    .text(getDate)
+    .attr("x", getpositionText)
+    .attr("y", getVertical)
+    .style("font-family", "'Roboto Mono', monospace")
+    .style("text-anchor", getpositionAnchor)
+    .style("font-size", "16px")
+    .style("fill", "white");
+
+  datagroups.append("text")
+    .attr("class", "Name")
+    .text(getMissionName)
+    .attr("x", gettextpositionText)
+    .attr("y", getVertical)
+    .style("text-anchor", getpositionAnchor)
+    .style("font-family", "'Roboto Mono', monospace")
+    .style("font-size", "18px")
+    .style("fill", "none")
+    .call(wrap, 200);
+
+  // datagroups.append("text")
+  //   .attr("class", "Detail")
+  //   .text(getDetailName)
+  //   .attr("x", gettextpositionText)
+  //   .attr("y", getDetailVertical)
+  //   .style("text-anchor", getpositionAnchor)
+  //   .style("font-family", "monospace")
+  //   .style("font-size", "15px")
+  //   .style("fill", "none")
+  //   .call(wrap, 300);
+
+
+  datagroups.append("circle")
+    .attr("cx", w / 2 +200)
+    .attr("cy", getVertical)
+    .attr("r", 10)
+    .style("fill", "white");
+
+  datagroups
+    .on("mouseover", function(d, i) {
+      d3.select(this).select("circle")
+        .transition()
+        .attr("r", 30)
+        .style("fill", "#625df6");
+      d3.select(this).select(".datetext")
+        .transition()
+        .style("fill", "#625df6");
+      d3.select(this).select(".Name")
+        .transition()
+        .duration(800)
+        .style("fill", "white")
+      // d3.select(this).select(".Detail")
+      //   .transition()
+      //   .duration(800)
+      //   .style("fill", "white")
+    })
+    .on("mouseout", function(d, i) {
+      d3.select(this).select("circle")
+        .transition()
+        .duration(800)
+        .attr("r", 10)
+        .style("fill", "white");
+      d3.select(this).select(".datetext")
+        .transition()
+        .duration(800)
+        .style("fill", "white");
+      d3.select(this).select(".Name")
+        .transition()
+        .duration(800)
+        .style("fill", "none")
+      // d3.select(this).select(".Detail")
+      //   .transition()
+      //   .duration(800)
+      //   .style("fill", "none")
+    });
+
+  // function nameMission(d, i) {
+  //   return "Name of the Mission: " + d.Name
+  //
+  // function mission(d, i) {
+  //   return "Mission Details: " + d.Mission
+  // }
+  //
+  // datagroups.append("text")
+  //   .text(nameMission)
+  //   .style("font-family", "monospace")
+  //   .attr("x", 20)
+  //   .attr("y", getVertical)
+  //   .style("fill", "black");
+  //
+  // datagroups.append("text")
+  //   .text(mission)
+  //   .style("font-family", "monospace")
+  //   .attr("x", 20)
+  //   .attr("y", getVertical)
+  //   .style("fill", "black");
+
+
+});
+
+
+
+
+
+//Launchsite
+let viz3 = d3.select("#launchsite")
+  .append("svg")
+  .attr("class", "viz4")
+  .attr("width", w)
+  .attr("height", h)
+  .style("background-color", "black");
+
+
+
+
+
+//Barchart
+let viz4 = d3.select("#barchart")
+  .append("svg")
+  .attr("class", "viz4")
+  .attr("width", w)
+  .attr("height", h)
   .style("background-color", "black");
 
 function gotData(incomingData) {
@@ -62,11 +302,11 @@ function gotData(incomingData) {
   console.log("minPop", minPop);
 
 
-  let datagroups = viz2.selectAll(".datagroup").data(incomingData).enter()
+  let datagroups2 = viz4.selectAll(".datagroup").data(incomingData).enter()
     .append("g")
     .attr("class", "datagroup");
 
-  let yScale = d3.scaleLinear().domain([0, 140]).range([0, h / 2]);
+  let yScale = d3.scaleLinear().domain([0, 140]).range([100, h / 2]);
   let colorScale = d3.scaleLinear().domain([0, 140]).range(["white", "#625df6"]);
 
   function getheight(d, i) {
@@ -75,25 +315,22 @@ function gotData(incomingData) {
   }
 
   function getyPosition(d, i) {
-    return -yScale(d.Launches.length);
+    return -(yScale(d.Launches.length) - 150);
   }
 
   function getColor(d, i) {
-    // if (d.name == "Shanghai Tower"){
-    //   return "yellow"
-    // }else{
-    //   return "black"
     return colorScale(d.Launches.length);
   }
 
-  let launchyears = datagroups
+  let launchyears = datagroups2
     .append("rect")
-    .attr("class", "launchyears")
+    .attr("class", "launchyear")
     .attr("x", 0)
     .attr("y", getyPosition)
-    .attr("width", 10)
+    .attr("width", 16)
     .attr("fill", getColor)
-    .attr("height", getheight);
+    .attr("height", getheight)
+  // .on("mouseover", handleMouseOver);
 
   function getName(d, i) {
     return d.name
@@ -103,34 +340,73 @@ function gotData(incomingData) {
     return d.Launches.length
   }
 
-  let lables = datagroups
+  let lables = datagroups2
     .append("text")
     .attr("class", "name")
     .text(getName)
-    .attr("x", 5)
-    .attr("y", -3)
+    .attr("x", 160)
+    .attr("y", -5)
     .attr("transform", "rotate(90)")
-    .style("font-family", "sans-serif")
+    .style("font-family", "'Roboto Mono', monospace")
+    .style("font-size", "20px")
     .style("fill", "white");
 
-  let numbers = datagroups
+  let numbers = datagroups2
     .append("text")
     .attr("class", "number")
     .text(getNumbers)
     .attr("x", 0)
-    .attr("y", 0)
-    .style("font-family", "sans-serif")
+    .attr("y", -500)
+    .style("font-family", "'Roboto Mono', monospace")
+    .style("font-size", "10px")
     .style("fill", "white");
 
   function getGroupPosition(d, i) {
     console.log("i is", i);
-    let x = (w / 120) * i;
+    let x = (w / 65) * i;
     let y = 600;
     return "translate(" + x + ", " + y + ")"
   }
 
-  datagroups.attr("transform", getGroupPosition);
+  datagroups2.attr("transform", getGroupPosition);
+
+  datagroups2
+    .on("mouseover", function(d, i) {
+      d3.select(this).select("rect")
+        .transition()
+        .attr("width", 18)
+        .attr("fill", "orange")
+    });
+
+
+  // var y_axis = d3.axisRight()
+  //   .scale(yScale)
+  //   .ticks(14)
+  //   .render()
+  //   .attr('transform', 'translate(0, 0)');
+  // datagroups2.call(y_axis);
 }
 
-
 d3.json("Data/Orbital_Launches.json").then(gotData);
+
+
+
+let viz5 = d3.select("#satellite")
+  .append("svg")
+  .attr("class", "viz5")
+  .attr("width", w)
+  .attr("height", h)
+  .style("background-color", "black");
+
+const circlePath = (angle, radius) => { // Calculate the angle at which the element will be placed.
+  let x = (radius * Math.cos(angle)) + 1000; // Calculate the x position of the element.
+  let y = (radius * Math.sin(angle)) + 200; // Calculate the y position of the element.
+  return "translate (" + x + ", " + y + " )"
+}
+
+// d3.json("Data/Satellites.csv").then(function(satelliteData) {
+//   console.log(satelliteData);
+//   let datagroups5 = viz5.selectAll(".satelliteData").data(satelliteData).enter()
+//     .append("g")
+//     .attr("class", "satelliteData");
+//   });
